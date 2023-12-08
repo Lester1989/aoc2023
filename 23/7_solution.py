5f6975b4c1476529a45240b18bbff92ceebd9ba2
Lester1989 <lester@techfak.uni-bielefeld.de>
2023-12-08 20:15:00 +0100

    solution 7 and 8

--- 23/7_solution.py
+++ 23/7_solution.py
@@ -2,24 +2,103 @@
 from __future__ import annotations
 
 import pathlib
+import collections
+from enum import Enum
+ordering = '23456789TJQKA' 
+ordering2 = 'J23456789TQKA' 
+
+class HandType(Enum):
+    HIGH_CARD = 0
+    ONE_PAIR = 1
+    TWO_PAIR = 2
+    THREE_OF_A_KIND = 3
+    FULL_HOUSE = 4
+    FOUR_OF_A_KIND = 5
+    FIVE_OF_A_KIND = 6
 
 def part_one(data:str):
     result = 0
+    hands = []
     for line in data.splitlines():
-        print(line)
+        hand, bid = line.split()
+        values = dict(collections.Counter(hand))        
+        if len(values) == 1:
+            hands.append((HandType.FIVE_OF_A_KIND, hand, bid))
+        elif len(values) == 2:
+            if max(values.values()) == 4:
+                hands.append((HandType.FOUR_OF_A_KIND, hand, bid))
+            else:
+                hands.append((HandType.FULL_HOUSE, hand, bid))
+        elif len(values) == 3:
+            if max(values.values()) == 3:
+                hands.append((HandType.THREE_OF_A_KIND, hand, bid))
+            else:
+                hands.append((HandType.TWO_PAIR, hand, bid))
+        elif len(values) == 4:
+            hands.append((HandType.ONE_PAIR, hand, bid))
+        else:
+            hands.append((HandType.HIGH_CARD, hand, bid))
+    
+    ordered_hands = sorted(hands, key=lambda x: (x[0].value, ordering.index(x[1][0]), ordering.index(x[1][1]), ordering.index(x[1][2]), ordering.index(x[1][3]), ordering.index(x[1][4])))
+    for idx, hand in enumerate(ordered_hands):
+        print(idx+1, hand)
+        result += (idx+1)*int(hand[2])
+
     print(result)
 
 def part_two(data:str):
     result = 0
+    hands = []
     for line in data.splitlines():
-        print(line)
+
+        hand, bid = line.split()
+        print(hand)
+        values = dict(collections.Counter(hand))
+        if 'J' in values and len(values) >= 2:
+            # increase hightest non-joker
+            joker_count = values.pop('J')
+           
+            highest = max(values.values())
+            print(f'found {joker_count} jokers, increasing {highest}')
+            for card, count in values.items():
+                if count == highest:
+                    print(f'increasing {card} by {joker_count}')
+                    values[card] += joker_count
+                    break
+
+        if len(values) == 1:
+            hands.append((HandType.FIVE_OF_A_KIND, hand, bid))
+        elif len(values) == 2:
+            if max(values.values()) == 4:
+                hands.append((HandType.FOUR_OF_A_KIND, hand, bid))
+            else:
+                hands.append((HandType.FULL_HOUSE, hand, bid))
+        elif len(values) == 3:
+            if max(values.values()) == 3:
+                hands.append((HandType.THREE_OF_A_KIND, hand, bid))
+            else:
+                hands.append((HandType.TWO_PAIR, hand, bid))
+        elif len(values) == 4:
+            hands.append((HandType.ONE_PAIR, hand, bid))
+        else:
+            hands.append((HandType.HIGH_CARD, hand, bid))
+    
+    ordered_hands = sorted(hands, key=lambda x: (x[0].value, ordering2.index(x[1][0]), ordering2.index(x[1][1]), ordering2.index(x[1][2]), ordering2.index(x[1][3]), ordering2.index(x[1][4])))
+    for idx, hand in enumerate(ordered_hands):
+        print(idx+1, hand)
+        result += (idx+1)*int(hand[2])
+
     print(result)
 
-EXAMPLE = """"""
+EXAMPLE = """32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"""
 
 if __name__ == "__main__":
     input_data = pathlib.Path('23/7_data.txt').read_text(encoding='utf-8')
-    input_data = EXAMPLE
-    part_one(input_data)
+    # input_data = EXAMPLE
+    # part_one(input_data)
     print('--- PART TWO ---')
     part_two(input_data)
--- 23/8_solution.py
+++ 23/8_solution.py
@@ -2,24 +2,72 @@
 from __future__ import annotations
 
 import pathlib
+import math
 
 def part_one(data:str):
-    result = 0
-    for line in data.splitlines():
-        print(line)
-    print(result)
+    instructions = data.splitlines()[0]
+    desert_map:dict[str,tuple[str,str]] = {}
+    for line in data.splitlines()[2:]:
+        node, left_right = line.split(' = ')
+        desert_map[node] = tuple(left_right.strip('()').split(', '))
+    
+    instructions_count = len(instructions)
+    location = 'AAA'
+    steps = 0
+    while location != 'ZZZ':
+        location = desert_map[location][0] if instructions[steps%instructions_count] == 'L' else desert_map[location][1]
+        steps += 1
+    print(steps)
 
 def part_two(data:str):
-    result = 0
-    for line in data.splitlines():
-        print(line)
-    print(result)
+    instructions = data.splitlines()[0]
+    desert_map:dict[str,tuple[str,str]] = {}
+    z_enders = []
+    locations:dict[str,str] = {}
+    for line in data.splitlines()[2:]:
+        node, left_right = line.split(' = ')
+        desert_map[node] = tuple(left_right.strip('()').split(', '))
+        if node[2] =="A":
+            locations[node] = node
+        elif node[2] == "Z":
+            z_enders.append(node)
+    
+    ghost_hits = []
+    for location in locations:
+        ghost_location = location
+        hits = []
+        for ghost_step,instruction in enumerate( instructions*100):
+            ghost_location = desert_map[ghost_location][0] if instruction == 'L' else desert_map[ghost_location][1]
+            if ghost_location[2]=="Z":
+                hits.append(ghost_step+1)
+                break
+        ghost_hits.append(hits)
+    print(math.lcm(*[hits[0] for hits in ghost_hits]))
 
-EXAMPLE = """"""
+EXAMPLE = """RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"""
+
+EXAMPLE2 = """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
 
 if __name__ == "__main__":
     input_data = pathlib.Path('23/8_data.txt').read_text(encoding='utf-8')
-    input_data = EXAMPLE
-    part_one(input_data)
+    # input_data = EXAMPLE2
+    # part_one(input_data)
     print('--- PART TWO ---')
     part_two(input_data)
